08a43cd67320e27d66c7bd74dfb02ef6b670580b
benjamindemoor <demoor.benjamin@gmail.com>
2015-01-03 15:04:54 +0100

    bourbon and sass added

--- gulpFile.js
+++ gulpFile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
 
 gulp.task('default', function() {
     gulp.watch('src/js/**/*.js',['scripts']);
+    gulp.watch('src/sass/**/*.scss',['sass']);
 });
 
-// Basic usage
 gulp.task('scripts', function() {
     gulp.src('src/js/app.js')
         .pipe(browserify({
@@ -14,4 +18,16 @@ gulp.task('scripts', function() {
             debug : true
         }))
         .pipe(gulp.dest('./public/'))
+});
+
+gulp.task('sass', function () {
+    gulp.src('src/sass/main.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: require('node-bourbon').includePaths
+        }))
+        .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./public/'));
 });
